ab17d37fc3a18925022323d438c7c2ef6240ce60
hyunhu <hyunhu@roundsquare.ai>
2026-05-06 09:47:20 +0900

    exchangereate Test code 추가, default 값 수정

--- src/test/java/com/airplanehome/flight/service/ExchangeRateServiceTest.java
+++ src/test/java/com/airplanehome/flight/service/ExchangeRateServiceTest.java
@@ -1,11 +1,26 @@
 package com.airplanehome.flight.service;
 
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import org.apache.http.client.HttpClient;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.client.ExpectedCount.once;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
@@ -37,6 +52,32 @@ class ExchangeRateServiceTest {
 
         assertEquals(1300d, service.getUsdToKrwRate());
         server.verify();
+    }
+
+    @Test
+    void shouldFetchRealUsdToKrwRate() throws Exception {
+        TrustStrategy trustAll = (X509Certificate[] chain, String authType) -> true;
+        SSLContext sslContext = SSLContextBuilder.create().loadTrustMaterial(null, trustAll).build();
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", sslSocketFactory)
+                .build();
+        HttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(sslSocketFactory)
+                .setConnectionManager(new PoolingHttpClientConnectionManager(registry))
+                .build();
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+
+        ExchangeRateService service = new ExchangeRateService(
+                restTemplate,
+                "https://api.exchangerate-api.com/v4/latest/USD",
+                60,
+                1450d);
+
+        double rate = service.getUsdToKrwRate();
+        System.out.println(">>> 현재 USD->KRW 환율: " + rate);
+        assertTrue(rate > 0, "환율은 0보다 커야 합니다");
     }
 
     @Test
